48c39eb85d8bee59b4f62e3286b1c99d2e21080a
ConnorMac <cmacdougall78@gmail.com>
2018-08-17 18:38:40 +0200

    Added basic IPFS support

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# stellar-non-fungible-factory
-Soon to be library for generating a single use once off token on the Stellar network
+# Stellar NFT
+
+The NFT library creates a non-fungible token on the Stellar network. The resulting token
+is a 0.0000001 supply limited token from a locked Issuer account that has an IPFS hash attached to it's account data. (Although it is not pinned so will be lost if your node goes
+offline)
+
+Currently the library is not opinionated about the data expected in the metadata.
+
+## Basic usage
+
+Before running the script you will need a funded Issuer account (1.5XLM) and a funded initial owner account (1 XLM). NOTE: that the Issuer account will be locked so it will not be usable after this.
+
+The machine running the script will also need an IPFS daemon running on localhost.
+
+```
+from stellar-token-factory import TokenFactory
+
+issuer_secret = 'SBLVL4PBIGHTCPEAWEWNZ2BSW2OU4KEUYHARWSL2AICCVNKQ6FQSP2OF'
+owner_secret = 'SAAXGVL7A56BS2CP5OWLREAVTVSFB2NAMIPZWC5ETMDCZ54A6UNTVUCZ'
+tf = TokenFactory(issuer_secret)
+tx = tf.generate_non_fungible_token(owner_secret, 'FUNGI', metadata={'img':'testing.jpg'})
+```
--- stellar-token-factory.py
+++ stellar-token-factory.py
@@ -1,42 +1,63 @@
+import io
+import json
+import ipfsapi
+import time
 from stellar_base.builder import Builder
 from stellar_base.keypair import Keypair
 
 
 class TokenFactory(object):
 
-    def __init__(self, issuer_secret, network='test'):
+    def __init__(self, issuer_secret, network='TESTNET'):
         self.issuer_secret = issuer_secret
         self.issuer_keypair = Keypair.from_seed(issuer_secret)
         self.network = network
+        # For now we default to a local running node
+        self.ipfs_api = ipfsapi.connect('127.0.0.1', 5001)
 
     def generate_non_fungible_token(self, initial_owner_secret, code, metadata=None):
         owner_keypair = Keypair.from_seed(initial_owner_secret)
         # First we generate the token trustline on the network
-        # TODO: Set limit to 0.0000001 as well
         self.create_trustline(initial_owner_secret, code)
         # Next we send exactly 0.0000001 to the initial owner
         return self.create_token_and_lock_account(
            code,
-           owner_keypair.address().decode()
+           owner_keypair.address().decode(),
+           metadata
         )
+
+    def create_ipfs_hash(self, data):
+        # TODO: Don't generate an actual file
+
+        with io.open(
+            'token' + str(int(time.time())) + '.json', 'w', encoding='utf-8'
+        ) as f:
+            numbytes = f.write(json.dumps(data))
+        res = self.ipfs_api.add('token.json')
+        return res.get('Hash')
 
     def create_trustline(self, secret, code):
         builder = Builder(secret=secret, network=self.network)
         builder.append_trust_op(
             self.issuer_keypair.address().decode(),
-            code
+            code,
+            limit='0.0000001'
         )
         builder.sign()
         return builder.submit()
 
-    def create_token_and_lock_account(self, code, owner_address):
+    def create_token_and_lock_account(self, code, owner_address, metadata):
+        # Setup the base transaction
         builder = Builder(
             secret=self.issuer_secret,
             network=self.network
         )
+        # Append relevant payment ops
+        ipfs_hash = self.create_ipfs_hash(metadata)
+        builder.append_manage_data_op('token_ipfs_hash', ipfs_hash)
         builder.append_payment_op(
             owner_address,
-            0.0000001,
+            '0.0000001',
             code,
             self.issuer_keypair.address().decode()
         )
